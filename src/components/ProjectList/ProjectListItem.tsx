86cfc31edec21051435742dc06830041cbb613a7
GadMinz <baykov97@mail.ru>
2022-11-30 17:07:17 +0300

    Added project links

--- src/components/ProjectList/ProjectListItem.tsx
+++ src/components/ProjectList/ProjectListItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./ProjectList.module.scss";
 import GlobalSvgSelector from "../../assets/icons/GlobalSvgSelector";
+import { Link } from "react-router-dom";
 
 interface ProjectListItemProps {
   id: string;
@@ -10,7 +11,9 @@ interface ProjectListItemProps {
 const ProjectListItem: React.FC<ProjectListItemProps> = ({ id, title }) => {
   return (
     <div className={s.project}>
-      <div className={s.project_title}>{title}</div>
+      <Link to={"/" + id} className={s.project_title}>
+        {title}
+      </Link>
       <button className={s.project_delete}>
         <GlobalSvgSelector id="x" />
       </button>
