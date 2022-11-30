dfa70ed089cc2376c1500f6f7d1e951687492305
GadMinz <baykov97@mail.ru>
2022-11-30 22:44:51 +0300

    Add dynamic title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import s from "./Header.module.scss";
+import { useAppSelector } from "../../hook";
+import {Link, useLocation} from "react-router-dom";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
+  let { title } = useAppSelector((state) => state.project.project);
+  if (useLocation().pathname === "/") {
+    title = `ToDo's`;
+  }
+  document.title = title;
   return (
     <div className={s.header}>
-      <div className={s.header_title}>{`ToDo's`}</div>
+      <Link to="/" className={s.header_title}>
+        {title}
+      </Link>
     </div>
   );
 };
